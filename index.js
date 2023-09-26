0bc145f8dbfc9cb0c9023109b3702603a3dbebb9
Alex Purfield <alexpurfield@gmail.com>
2023-09-25 23:34:55 -0600

    added to shapes classes so they have the specific descriptions of their svg shapes, brought in shapes to index from shapes, working on writeToFile function- need help with error that keeps coming up after user input

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require ("inquirer"); //need inquirer for user input 
 const fs = require ("fs"); //will need fs to make svg file 
+const Triangle = require("./lib/shapes");
+const Circle = require("./lib/shapes");
+const Square = require("./lib/shapes");
+let answers = "";
 
 //questions for user input
 const questions = () => {
@@ -32,17 +36,53 @@ const questions = () => {
 
     ])
 
-
-    
-};
-module.exports = questions;
-questions();
+    .then((answers) => {
+        if (answers.text.length > 3) {
+            console.log("Can only use up to 3 characters");
+            questions();
+        } else {
+            writeToFile("logo.svg", answers);
+        }
+        }
+    );
+}
 
 //Need function that will write SVG file using user input from inquirer
 
 function writeToFile(fileName, answers) {
+    //start with empty string for input 
     let svgString = "";
-    svgString = '<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">';
+    // set the container for logo
+    svgString = '<svg width="300" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">';
     svgString += '<g>';
     svgString += `${answers.shape}`;
-};
+    //choice of triangle, square, or circle- set the svg html details
+let userShape;
+if (answers.shape === "Triangle") {
+userShape = new Triangle(); //create new triangle object
+svgString += `<polygon points = "150, 18 244, 182 56, 182'; fill ="${answers.shapeColor}"/>`; 
+console.log (userShape);
+} else if (answers.shape === "Square") {
+userShape = new Square(); //create new square object if choose square
+svgString += `<rect x="10" y="10" width="30" height="30"; fill="${answers.shapeColor}"/> `;
+    console.log (userShape);
+} else (answers.shape === "Circle" ); {
+   let userShape = new Circle (); //create new circle object if don't choose others 
+   console.log (userShape);
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`}
+
+      // <text> tag gives rise to text alignment, text-content/text-color taken in from user input and gives default font size of "40"
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  // Closing </g> tag
+  svgString += "</g>";
+  // Closing </svg> tag
+  svgString += "</svg>";
+
+  // Using file system module to generate svg file, takes in file name given in the promptUser function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
+}
+
+
+questions();
